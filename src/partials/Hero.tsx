19c1c4b73ec36a8d5370cf265a922e31e3d62863
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-04-10 08:46:45 +0200

    start customizing the project

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => (
     <HeroAvatar
       title={
         <>
-          Hi there, I'm <GradientText>Ixartz</GradientText> 👋
+          Hi there, I'm <GradientText>UltraViolet 33</GradientText> 👋
         </>
       }
       description={
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
-  locale_region: 'en-us',
-  locale: 'en',
+  site_name: 'UltraViolet33 blog and portfolio',
+  title: 'UltraViolet33 blog and portfolio',
+  description: 'My portfolio and personal blog',
+  author: 'UltraViolet33',
+  locale_region: 'fr',
+  locale: 'fr',
 };
