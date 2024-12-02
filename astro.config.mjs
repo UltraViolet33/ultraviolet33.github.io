8e60ba37932b629aad177ac4730b447e31f236f4
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-12-02 11:59:10 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import solidJs from "@astrojs/solid-js"
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://ultraviolet33.github.io/",
+  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+})
